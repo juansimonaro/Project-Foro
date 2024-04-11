46299db35918ae9179819a8e27fe433be01f5fb2
Juan <simonarocavedonijupe@uvm.edu.ve>
2024-04-11 04:53:56 -0400

    bd

--- back/database/db.js
+++ back/database/db.js
@@ -7,5 +7,5 @@ export const sql = createPool({
     host: process.env.LOCAL_HOST,
     user: process.env.USER,
     password: process.env.PASS,
-    database: process.env.DATA_BASE
+    database: ""
 })
